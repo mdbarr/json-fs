6b25ebc553a5c8fafaa45850902363d34995acdd
Mark Barr <mark@hyperingenuity.com>
2019-03-18 23:21:29 -0400

    json-fs: shims for object resolution added

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 #!/usr/bin/env node
 'use strict';
+
+//////////
+// Shims
+
+Object.defineProperty(String.prototype, 'stripWhitespace', {
+  value: function() {
+    return this.replace(/\s/g, '');
+  },
+  enumerable: false,
+  configurable: true
+});
+
+Object.resolve = function(object, path) {
+  const parts = path.stripWhitespace().split(/\./);
+
+  for (const part of parts) {
+    object = object[part];
+    if (!object) {
+      return undefined;
+    }
+  }
+  return object;
+};
+
+//////////
+// Loader
